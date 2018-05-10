b8d70f6f2b570b29be85c4d807babd724879f991
ZENG <zeng@ZENGs-iMac.local>
2018-05-10 20:14:22 +0800

    fix

--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -132,7 +132,7 @@ class LoanController extends Controller
             $this->brokerage($swap);
         }
     }
-    public function getUsers($user,$data=[])
+    public function getUsers($user,&$data=[])
     {
         if (!empty($user)){
             if ($user->proxy_id!=0){
@@ -144,7 +144,7 @@ class LoanController extends Controller
             }
         }
 //        dd($data);
-        var_dump($data);
+//        var_dump($data);
 //        die(1);
         return $data;
     }
