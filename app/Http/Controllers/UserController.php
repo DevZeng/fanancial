ee2ff33dd0ad7161d8c364dc5c3f5015f86fceb9
ZENG <zeng@ZENGs-iMac.local>
2018-05-20 10:16:24 +0800

    fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -639,8 +639,7 @@ class UserController extends Controller
         if (!empty($data)){
             foreach ($data as $datum){
                 $role = RoleUser::where('user_id','=',$datum->id)->pluck('role_id')->first();
-                dd($role);
-                $datum->role = Role::find($role)->display_name;
+                $datum->role = empty($role)?Role::find($role)->display_name:'无权限';
             }
         }
         return response()->json([
