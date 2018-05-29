0be1a4512107f9a4fea55ff2f95cb8eae520208f
ZENG <zeng@ZENGs-iMac.local>
2018-05-29 11:03:59 +0800

    fix

--- app/Http/Middleware/CrossDomain.php
+++ app/Http/Middleware/CrossDomain.php
@@ -19,14 +19,14 @@ class CrossDomain
         $origin = config('allow');
         if (in_array($request->getSchemeAndHttpHost(),$origin)){
             if($request->getMethod() === 'OPTIONS'){
-                $response->header('Access-Control-Allow-Origin', env('CLIENT'));
+                $response->header('Access-Control-Allow-Origin', $request->getSchemeAndHttpHost());
 //            $response->header('Access-Control-Allow-Origin', '*');
                 $response->header('Access-Control-Allow-Headers', 'Origin, Access-Control-Request-Headers, SERVER_NAME, Access-Control-Allow-Headers, cache-control, token, X-Requested-With, Content-Type, Accept, Connection, User-Agent, Cookie, X-XSRF-TOKEN');
                 $response->header('Access-Control-Allow-Methods', 'OPTIONS,GET, POST, PATCH, PUT, DELETE');
                 $response->header('Access-Control-Allow-Credentials', 'true');
             }else{
 //            $response->header('Access-Control-Allow-Origin', '*');
-                $response->header('Access-Control-Allow-Origin', env('CLIENT'));
+                $response->header('Access-Control-Allow-Origin', $request->getSchemeAndHttpHost());
                 $response->header('Access-Control-Allow-Headers', 'Origin, Access-Control-Request-Headers, SERVER_NAME, Access-Control-Allow-Headers, cache-control, token, X-Requested-With, Content-Type, Accept, Connection, User-Agent, Cookie, X-XSRF-TOKEN');
                 $response->header('Access-Control-Allow-Methods', 'OPTIONS,GET, POST, PATCH, PUT, DELETE');
                 $response->header('Access-Control-Allow-Credentials', 'true');
