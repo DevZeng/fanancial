ed16263fcb209c2688ce09c75b20b2ea4746504f
DevZeng <zengshungeng@live.com>
2018-05-31 16:01:03 +0800

    fix

--- config/services.php
+++ config/services.php
@@ -37,6 +37,6 @@ return [
     'weixin' => [
         'client_id' => 'wx51669b8a457b05ca',
         'client_secret' => '61ee0a22bce63c7d403448e3f6ab1bc7',
-        'redirect' => env('WEIXIN_REDIRECT_URI'),
+        'redirect' => 'https://www.gzzrdc.com/api/weixin/callback',
     ],
 ];
