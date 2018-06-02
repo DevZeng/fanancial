d301d8eb0c16c8873bc6d7c56f3996c545bc1410
ZENG <zeng@ZENGs-iMac.local>
2018-06-02 10:38:27 +0800

    fix

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Libraries\Wxxcx;
+use App\Models\ProxyApply;
+use App\Models\WeChatUser;
 use Illuminate\Http\Request;
 
 class WeChatController extends Controller
@@ -16,10 +18,38 @@ class WeChatController extends Controller
         $wechat = new Wxxcx(config('wxxcx.app_id'),config('wxxcx.app_secret'));
         $data = $wechat->request($url);
         $url2 = 'https://api.weixin.qq.com/sns/userinfo?access_token=%s&openid=%s&lang=zh_CN';
-        var_dump($data);
         $url2 = sprintf($url2,$data['access_token'],$data['openid']);
-        echo $url2;
+//        var_dump($data);
+
+//        echo $url2;
         $userData = $wechat->request($url2);
-        dd($userData);
+        $user = WeChatUser::where('open_id','=',$userData['openid'])->first();
+        if (empty($user)){
+            $user = new WeChatUser();
+            $user->open_id = $userData['openid'];
+            $user->nickname = $userData['nickname'];
+            $user->avatarUrl = $userData['headimgurl'];
+            if ($post->proxyid){
+                $user->proxy_id = $post->proxyid;
+            }
+            $user->save();
+            $user->apply = 0;
+            $token = CreateNonceStr(10);
+            setUserToken($token,$user->id);
+        }else{
+//                $user->flag = 1;
+//                $user->save();
+            $user->apply = ProxyApply::where('user_id','=',$user->id)->where('state','!=',2)->count();
+            $token = CreateNonceStr(10);
+            setUserToken($token,$user->id);
+        }
+        return response()->json([
+            'msg'=>'ok',
+            'data'=>[
+                'token'=>$token,
+                'user'=>$user
+            ]
+        ]);
+//        dd($userData);
     }
 }
