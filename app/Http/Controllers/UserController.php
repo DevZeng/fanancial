510f477ecb96d57210d4fda76f9107d56afc3861
ZENG <zeng@ZENGs-iMac.local>
2018-06-02 19:47:03 +0800

    fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -374,8 +374,8 @@ class UserController extends Controller
 //        $app = new Wxxcx(config('wxxcx.app_id'),config('wxxcx.app_secret'));
 //        $access_token = $app->getAccessToken();
 //        dd($access_token);
-        $uid = 1;
-//        $uid = getUserToken(Input::get('token'));
+//        $uid = 1;
+        $uid = getUserToken(Input::get('token'));
         $url = 'http://app.gzzrdc.com/?proxyid='.$uid;
 //        header('content-type:image/png');
 //        return QrCode::format('png')->size(200)->generate('http://laravelacademy.org');
