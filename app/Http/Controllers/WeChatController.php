1ab224ea612517f45ecc468400174b9baeeff7c3
ZENG <zeng@ZENGs-iMac.local>
2018-06-06 15:05:43 +0800

    fix

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -103,7 +103,7 @@ class WeChatController extends Controller
             $requestUri = sprintf($requestUri,$data['access_token'],$user->open_id);
             $returnData = $wechat->request($requestUri);
         }
-        return response()->json($returnData);
+//        return response()->json($returnData);
         if (isset($returnData)){
             return response()->json([
                 'msg'=>'ok',
