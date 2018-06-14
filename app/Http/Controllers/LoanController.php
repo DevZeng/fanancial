ee00297f114b0e54498e4b227d582e9fd603ee93
ZENG <zeng@ZENGs-iMac.local>
2018-06-14 15:32:22 +0800

    fix

--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -442,7 +442,7 @@ class LoanController extends Controller
             $db->whereIn('proxy_id',$idArr);
         }
         if ($name){
-            $idArr = WeChatUser::where('name','like','%'.$name.'%')->pluck('id')->toArray();
+            $idArr = WeChatUser::where('nickname','like','%'.$name.'%')->pluck('id')->toArray();
             $db->whereIn('proxy_id',$idArr);
         }
         if ($date){
